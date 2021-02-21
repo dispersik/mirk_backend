09323d8e3eb5d6f0376a11b7410de327fc40bae1
dispersik <korotkmax@gmail.com>
2021-02-21 16:51:51 +0300

    change port

--- main.go
+++ main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "encoding/json"
-    // "github.com/gorilla/mux"
-    "time"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	// "github.com/gorilla/mux"
+	"time"
 )
 
 type Message struct {
-    Text string `json:"text"`
-    Dt time.Time `json:"dt"`
-    Source User `json:"source"`
-    Destination User `json:"destination"`
+	Text        string    `json:"text"`
+	Dt          time.Time `json:"dt"`
+	Source      User      `json:"source"`
+	Destination User      `json:"destination"`
 }
 
 type User struct {
-	Id int `json:"id"`
+	Id       int    `json:"id"`
 	Nickname string `json:"nickname"`
 }
 
 var Messages []Message
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllMessages(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Endpoint Hit: returnAllMessages")
-    json.NewEncoder(w).Encode(Messages)
+func returnAllMessages(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllMessages")
+	json.NewEncoder(w).Encode(Messages)
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage)
-    http.HandleFunc("/messages", returnAllMessages)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/messages", returnAllMessages)
+	log.Fatal(http.ListenAndServe(GetPort(), nil))
 }
 
 func main() {
@@ -47,5 +47,15 @@ func main() {
 	Messages = []Message{
 		Message{Text: "Hello world", Dt: now, Source: alice, Destination: bob},
 	}
-    handleRequests()
+	handleRequests()
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
